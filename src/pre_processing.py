91917e7c780597ac73f5e79f6e2f9aed33d5aa1f
lennonay <lennon.auyeung@me.com>
2022-11-22 17:22:48 -0800

    read csv

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -6,16 +6,11 @@ from sklearn.model_selection import train_test_split
 
 opt = docopt(__doc__)
 
-def main(data_location, test_size, output_location):
+def main(data_location):
     
     maternal_risk_df = pd.read_csv(data_location)
     
-    train_df, test_df = train_test_split(maternal_risk_df, test_size=test_size, random_state=123)
-    
-    train_df.to_csv(output_location+'train_df.csv', index = False)
-    test_df.to_csv(output_location+'test_df.csv', index = False)
-
-
+    train_df, test_df = train_test_split(maternal_risk_df, test_size=test_size, random_state=123)  
 
 if __name__ == "__main__":
-  main(opt["--data_location"], opt["--test_size"], opt["--output_location"])
+  main(opt["--data_location"])
